fa9081ab7be0c07e44bdbbcf9621804f2080788c
Shim2003 <shimkh-wm23@student.tarc.edu.my>
2025-07-14 14:42:17 +0800

    import csv

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#Hello mother fucker
+import pandas as pd
+
+from sklearn.neighbors import KNeighborsClassifier
+
+data = pd.read_csv('heart_statlog_cleveland_hungary_final.csv')
+
+print(data.head())
+
+#checking for null values
+print(data.isnull().sum())
